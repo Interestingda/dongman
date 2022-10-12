c168bd67d3f606abb552836d03dcf2e84edecdda
diss <1219276869@qq.com>
2022-10-12 22:36:46 +0800

    创建项目

---
+++ domanservice/src/main/java/com/wuxin/domanservice/DomanserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wuxin.domanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomanserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomanserviceApplication.class, args);
+    }
+
+}
---
+++ domanservice/src/test/java/com/wuxin/domanservice/DomanserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuxin.domanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomanserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
